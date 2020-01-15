e52652f1fd47e61198914a7ecef6592a8efe95de
julio.ramos <julio.ramos@diginteligencia.com>
2020-01-15 10:24:43 -0600

    upload build

---
+++ docs/precache-manifest.c1de8fb0778b5b3831472631dbba99a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b457d6d343f613addfb54984520672e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f9e0b8036ea933e5313",
+    "url": "/static/css/main.5f61b0ac.chunk.css"
+  },
+  {
+    "revision": "61080e2eef506ee952f0",
+    "url": "/static/js/2.db52079a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.db52079a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f9e0b8036ea933e5313",
+    "url": "/static/js/main.cbb58303.chunk.js"
+  },
+  {
+    "revision": "0cf2facd8b0c204c49ca",
+    "url": "/static/js/runtime-main.49ae518b.js"
+  }
+]);
