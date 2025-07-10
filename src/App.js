f1f59152c127120b4136a51a8848d654ee9ac63e
Julio Ramos <ramos.isw@gmail.com>
2025-07-09 18:24:21 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faTwitter, faStackOverflow, faDev, faMedium, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import Typing from 'react-typing-animation';
+import { faGithub, faTwitter, faStackOverflow, faDev, faMediumM, faLinkedin, faHackerrank } from '@fortawesome/free-brands-svg-icons'
+import { TypeAnimation } from 'react-type-animation';
 
 import './App.css';
 
@@ -9,29 +9,29 @@ function App() {
   return (
     <main>
       <div class="intro">
-        <Typing speed={80}>
-          Hello, I'm
-          <Typing.Speed ms={200} />
-          <span> </span>
-          Developer
-          <Typing.Speed ms={50} />
-          <Typing.Backspace count={9} />
-          <Typing.Delay ms={500} />
-          <Typing.Speed ms={100} />
-          Julio Ramos
-        </Typing>
+        <TypeAnimation
+          sequence={[
+            'Hello, I\'m developer',
+            1000,
+            'Hello, I\'m Julio Ramos',
+          ]}
+          wrapper="span"
+          cursor={false}
+          speed={50}
+        />
       </div>
       <div class="tagline">Full Stack Dev | Open-Source & Photography Enthusiast | Software Engineer</div>
       <div class="icons-social">
         <a target="_blank" rel="noopener noreferrer" href="https://github.com/ramosisw"><FontAwesomeIcon icon={faGithub} /></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.hackerrank.com/profile/JulioRamos0"><FontAwesomeIcon icon={faHackerrank} /></a>
         <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/ramosisw"><FontAwesomeIcon icon={faTwitter} /></a>
         <a target="_blank" rel="noopener noreferrer" href="https://stackoverflow.com/story/ramosisw"><FontAwesomeIcon icon={faStackOverflow} /></a>
         <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ramosisw"><FontAwesomeIcon icon={faLinkedin} /></a>
         <a target="_blank" rel="noopener noreferrer" href="https://dev.to/ramosisw"><FontAwesomeIcon icon={faDev} /></a>
-        <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@ramosisw"><FontAwesomeIcon icon={faMedium} /></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@ramosisw"><FontAwesomeIcon icon={faMediumM} /></a>
       </div>
       <div align="center">
-        <img src="http://hits.dwyl.io/ramosiswgithubcom.svg"/>
+        {/* <img src="https://hits.dwyl.com/ramosisw/ramosiswgithubcom.svg"/> */}
       </div>
     </main>
   );
